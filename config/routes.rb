365c6b49f30f3855fdc47499c0030b1ef2444a5e
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-01-26 16:47:51 -0300

    set up project

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  get "users" => "users#index"
+  resources :sessions, only: [:create, :destroy]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+   root to: "static#home"
+end
